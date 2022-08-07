fcd3069bd5397c87a41c20394810555f40cb313b
shantanu <shantanumane01@gmail.com>
2022-08-07 14:55:46 +0530

    CORS_HEADER_ADDED

--- healthCard/settings.py
+++ healthCard/settings.py
@@ -27,6 +27,27 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS=True
+CORS_ALLOW_METHODS=[
+    "DELETE",
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "OPTIONS",
+]
+
+CORS_ALLOW_HEADERS=[
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 
 # Application definition
 
